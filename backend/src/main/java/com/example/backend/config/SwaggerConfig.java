bc2d137f66f1545d975ef43f2b4c7b40f80d24d8
김지원 <kjw3568@naver.com>
2026-01-27 11:03:10 +0900

    fix:cors설정추가

--- backend/src/main/java/com/example/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/example/backend/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package com.example.backend.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,9 @@ public class SwaggerConfig {
         String jwtSchemeName = "BearerAuth";
 
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/").description("Default Server URL")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
                 .components(
                         new Components()

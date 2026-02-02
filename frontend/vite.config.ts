41970b34114932e208c9756fa33082dc662ca26a
문희성 <heeseong2394@gmail.com>
2026-02-02 11:11:45 +0900

    feature/frontend/입양과정수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,9 +18,12 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
-      "/oauth2": "http://localhost:8080",
-      "/login": "http://localhost:8080",
+      "/api": 'https://i14c109.p.ssafy.io',
+      "/oauth2": 'https://i14c109.p.ssafy.io',
+      "/login": 'https://i14c109.p.ssafy.io',
+      // "/api": "http://localhost:8080",
+      // "/oauth2": "http://localhost:8080",
+      // "/login": "http://localhost:8080",
     },
   },
 })
